d591907fe504c24ca49719b6b6b544fd5480dd61
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-12-19 12:00:00 +0000

    Profile donezo

--- src/main/resources/static/public/js/setMaxDate.js
+++ src/main/resources/static/public/js/setMaxDate.js
@@ -1,9 +1,7 @@
-const setMaxDate = () => {
+export const setMaxDate = () => {
     const today = new Date();
     const eighteenYearsAgo = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
     const formattedDate = eighteenYearsAgo.toISOString().split('T')[0];
 
     $("#dateNaissance").attr("max", formattedDate);
-}
-
-export default setMaxDate;
+};
