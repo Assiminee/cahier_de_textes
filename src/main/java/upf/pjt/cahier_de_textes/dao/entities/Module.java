7b39272183f5b8004164567e6cf97288084602d7
Raniaabouabid1 <122936089+Raniaabouabid1@users.noreply.github.com>
2024-12-22 22:29:23 +0100

    k[A

--- src/main/java/upf/pjt/cahier_de_textes/dao/entities/Module.java
+++ src/main/java/upf/pjt/cahier_de_textes/dao/entities/Module.java
@@ -5,38 +5,52 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import upf.pjt.cahier_de_textes.dao.entities.enumerations.ModeEval;
+
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "module")
 public class Module {
+    public Module() {}
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-
+    @Getter
+    @Setter
     @NotBlank
     @Column(name = "intitule", nullable = false)
     private String intitule;
 
+    @Getter
+    @Setter
     @NotNull
     @Column(name = "nombre_heures", nullable = false)
     private int nombre_heures;
 
-    @NotNull
+
+    @Setter
     @Enumerated(EnumType.STRING)
     @Column(name = "mode_evaluation", nullable = false, columnDefinition = "VARCHAR(255) DEFAULT 'EXAM'")
     private ModeEval modeEvaluation;
 
+    public String getModeEvaluation() {
+        return modeEvaluation.toString();
+    }
+
+
+    @Getter
+    @Setter
     @ManyToOne
     @JoinColumn(name = "responsable")
     @JsonBackReference
     private Professeur responsable;
+
+    public String getResponsableNom() {
+        return responsable.nom;
+    }
 }
